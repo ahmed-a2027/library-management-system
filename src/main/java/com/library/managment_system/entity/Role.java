ca97fee727e94d327a1a668af4d557355bea9673
Ahmed Abdelhalem <ahmedabdelhalemmohamed2027@gmail.com>
2026-09-20 16:07:08 +0300

    create role entity and add database properties

--- src/main/java/com/library/managment_system/entity/Role.java
+++ src/main/java/com/library/managment_system/entity/Role.java
@@ -1,4 +1,26 @@
 package com.library.managment_system.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Role {
+
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
 }
