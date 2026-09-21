e372d2b7c86de5aec6849d4815092b61abae38c3
Ahmed Abdelhalem <ahmedabdelhalemmohamed2027@gmail.com>
2026-09-22 02:40:14 +0300

    add error response and updates global exception

--- src/main/java/com/library/managment_system/exception/GlobalExceptionHandler.java
+++ src/main/java/com/library/managment_system/exception/GlobalExceptionHandler.java
@@ -1,15 +1,25 @@
 package com.library.managment_system.exception;
 
+import com.library.managment_system.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
-    public ResponseEntity<String> handleEmailAlreadyExists(EmailAlreadyExistsException exception) {
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
+    public ResponseEntity<ErrorResponse> handleEmailAlreadyExists(EmailAlreadyExistsException exception) {
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .status(HttpStatus.CONFLICT.value())
+                .message(exception.getMessage())
+                .timestamp(LocalDateTime.now())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
     }
 }
